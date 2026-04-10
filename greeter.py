da46bea706ef62d88a5b263dd52fb2532b1a4cc4
Chuansong Zhou <zhouchua@flip3.engr.oregonstate.edu>
2026-04-09 22:39:54 -0700

    finish assignment1

--- greeter.py
+++ greeter.py
@@ -3,15 +3,15 @@ class Greeter:
     def __init__(self, input_name: str, input_place: str) -> None:
         # build an object contstructor that will store the input name and place
         # into object attributes for 'name' and 'place'.
-        pass
+        self.name = input_name
+        self.place = input_place
 
     def greeting(self) -> None:
         # function to *print* a customized greeting greeting the person 
         # identified by the value in the 'name' attribute, and welcoming them 
         # to the value in the 'place' attribute
-        pass
+        print("Hello, " + self.name + ", welcome to " + self.place)
 
     def __str__(self) -> str:
         # implementaion for displaying the contents including name and place:
         return f'Name: {self.name}, Place: {self.place}'
-        pass
